db3da5215891e1595171b4edfb461fc87002136c
Luigi Indaco <luigindaco@gmail.com>
2024-11-19 11:55:52 +0100

    commit

--- fe/src/config/config.ts
+++ fe/src/config/config.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'your-elastic-beanstalk-url';
 
 export const apiConfig = {
   baseURL: API_URL,
